37ccc28e679fad4265f4156fde1ef761e4484dc3
hectoramadorglez <hectoramador2005@gmail.com>
2024-11-04 14:25:13 +0100

    asdfge

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Disparo : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] int velocidad;
+    [SerializeField] int danhoDisparo;
     void Start()
     {
         
@@ -15,4 +17,21 @@ public class Disparo : MonoBehaviour
     {
         transform.Translate(new Vector3(1,0,0)*3*Time.deltaTime);
     }
+    private void Movimiento() 
+    { 
+        transform.Translate(new Vector3(1,0,0)* velocidad*Time.deltaTime);
+    
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemigo")) 
+        {
+            Destroy(gameObject);
+            collision.gameObject.GetComponent<Enemigo>().RecibirDanho(danhoDisparo);
+
+
+
+        }
+    }
+    
 }
--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -6,19 +6,34 @@ public class Enemigo : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float velocidad;
+    [SerializeField] float vidas;
+    Animator animator;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movimiento();
+        
     }
     void Movimiento() 
     {
         transform.Translate(new Vector2(-1, 0).normalized * velocidad * Time.deltaTime);
 
+    }
+    public void RecibirDanho(float danhoRecibido) 
+    {
+        vidas -= danhoRecibido;
+        animator.SetTrigger("blink");
+        if (vidas <=0) 
+        { 
+            Destroy(gameObject);
+        
+        }
+        
+    
     }
 }
